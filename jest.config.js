43f2d58f41e77a9ef45d46f3dab7632da98ee099
Oleksii Kuznietsov <oleksii_kuznietsov@epam.com>
2025-09-08 21:19:08 +0300

    Added tests for lambda handlers

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  clearMocks: true,
 };
